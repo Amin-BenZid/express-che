dceb3cded8dc84d1d45683d59679da7ff1425744
Amin-BenZid <54577480+Amin-BenZid@users.noreply.github.com>
2022-01-17 23:56:06 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -10,9 +10,13 @@ app.set("view engine", "pug");
 
 var date = new Date();
 var time = date.toUTCString().split(" ");
+var array = arr.r(9, 9);
 var day = (time[0] = !"Sat" || "Sun" ? (day = true) : (day = false));
-var hours =
-  time[4] == arr.r(9, 17).toString() ? (hours = true) : (hours = false);
+var hours = array.includes(Number(time[4].substring(0, 2)))
+  ? (hours = true)
+  : (hours = false);
+console.log(hours);
+
 if (day && hours === true) {
   app.get("/", (req, res) => {
     res.render("index", { title: "Home" });
